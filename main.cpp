553c2cd527e41d47b8d1fa4fd14c654237c8cbe5
TigasGPT <tmdmiranda@gmail.com>
2024-12-05 10:21:38 +0000

    fix pls

--- main.cpp
+++ main.cpp
@@ -97,7 +97,10 @@ int main()
     float vertices[] = {
         -0.5f, -0.5f, 0.0f, // left
          0.5f, -0.5f, 0.0f, // right
-         0.0f,  0.5f, 0.0f  // top
+         0.0f,  0.5f, 0.0f, // top
+         0.5f, -0.5f, 0.0f,
+         1.0f, -0.5f, 0.0f,
+         0.5f, 0.5f, 0.0f
     };
 
     unsigned int VBO, VAO;
@@ -139,7 +142,7 @@ int main()
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
         // glBindVertexArray(0); // no need to unbind it every time
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
