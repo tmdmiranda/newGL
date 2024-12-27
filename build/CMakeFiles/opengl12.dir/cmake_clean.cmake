7bd5d743880dfd9b253d874e2a01d47ae51ad5b3
Tiago Miranda <tmdmiranda@gmail.com>
2024-12-27 03:01:48 +0000

    linux yay

---
+++ build/CMakeFiles/opengl12.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl12.dir/link.d"
+  "CMakeFiles/opengl12.dir/main.cpp.o"
+  "CMakeFiles/opengl12.dir/main.cpp.o.d"
+  "opengl12"
+  "opengl12.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/opengl12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
