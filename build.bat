f69c362c4b688ecd99585a4a9ca97e36d13a266c
Tiago Miranda <tmdmiranda@gmail.com>
2024-12-27 22:44:49 +0000

    small fix

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 mkdir build
 cd build
 
-cmake -DCMAKE_TOOLCHAIN_FILE=C:\Users\BigEp\DEVTOOLS\vcpkg\scripts\buildsystems\vcpkg.cmake ..
+cmake -DCMAKE_TOOLCHAIN_FILE=%VCPKG_ROOT%\scripts\buildsystems\vcpkg.cmake ..
 
 cmake --build . --config Debug
 
